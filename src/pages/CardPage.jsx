3df972793d600d6bf25077f1538d677b63424ebc
radionadejda <radionadejda@gmail.com>
2023-11-22 12:59:55 +0300

    start project

---
+++ src/pages/CardPage.jsx
@@ -0,0 +1,9 @@
+import '../styles/_App.scss';
+import '../styles/_CardPage.scss';
+
+// import { Button } from '../components/Button/Button.jsx';
+// import { words } from '../data/data.js';
+
+export function CardPage() {
+    return <div className="">card goes here</div>;
+}
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,6 @@
+import '../styles/_App.scss';
+import '../styles/_NotFoundPage.scss';
+
+export function NotFoundPage() {
+    return <div className="">you're lost, little girl</div>;
+}
