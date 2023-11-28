6f9fdd6de5f745c318b778f8b78ad35458c609be
radionadejda <radionadejda@gmail.com>
2023-11-28 14:35:29 +0300

    fix list page to use state words

--- src/pages/ListPage/ListPage.jsx
+++ src/pages/ListPage/ListPage.jsx
@@ -1,13 +1,12 @@
 import { Row } from '../../components/Row/Row';
-import words from '../../data/data.json';
 import styles from '../../styles/ListPage.module.scss';
 
-export function ListPage() {
+export function ListPage({ stateWords }) {
     const heading = { english: 'word', transcription: 'transcription', russian: 'translation', tags: 'tags' };
     return (
         <div className={styles.WordList}>
             <Row word={heading} isHeading={true} />
-            {words.map((word, id) => (
+            {stateWords.words.map((word, id) => (
                 <Row key={id} word={word} />
             ))}
         </div>
