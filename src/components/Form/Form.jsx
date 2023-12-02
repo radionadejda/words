8f26b98907a5d13d593d128fc7fd7edee108b156
radionadejda <radionadejda@gmail.com>
2023-12-02 21:29:22 +0300

    fix Form

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,18 +2,6 @@ import { useState } from 'react';
 import styles from './Form.module.scss';
 
 export const Form = ({ onClose, mode }) => {
-    //also should have onSave (to save changes) and onClear (to clear all inputs)
-    const [isOpen, setIsOpen] = useState(true);
-
-    const handleClose = () => {
-        setIsOpen(false);
-        onClose();
-    };
-
-    const handleButtonClick = (event) => {
-        event.preventDefault();
-    };
-
     let buttonText = '';
     let formTitle = '';
 
@@ -36,15 +24,13 @@ export const Form = ({ onClose, mode }) => {
 
     return (
         <div>
-            {isOpen && (
-                <div>
-                    <h2>{formTitle}</h2>
-                    <form>
-                        <button onClick={handleButtonClick}>{buttonText}</button>
-                    </form>
-                    <button onClick={handleClose}>Close</button>
-                </div>
-            )}
+            <div>
+                <h2>{formTitle}</h2>
+                <form>
+                    <button>{buttonText}</button>
+                </form>
+                <button onClick={onClose}>Close</button>
+            </div>
         </div>
     );
 };
