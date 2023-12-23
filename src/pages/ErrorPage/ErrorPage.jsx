877532d21d81f1d1153b8d0e00490599f31f4ffa
radionadejda <radionadejda@gmail.com>
2023-12-23 16:33:46 +0300

    add gif to error page

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -3,7 +3,13 @@ import styles from '../../styles/ErrorPage.module.scss';
 export const ErrorPage = () => {
     return (
         <main className={styles.main}>
-            <div className={styles.div}>you're lost, little girl</div>
+            <div className={styles.div}>
+                <p>sry we don't have a page like this</p>
+                <img
+                    src="../../../public/error.gif"
+                    alt="cat-erroring"
+                />
+            </div>
         </main>
     );
 };
